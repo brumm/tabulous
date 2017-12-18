cd45f6d51b26142d80d71bd9775bb9e19fccc0b7
Philipp Brumm <philipp.brumm@gmail.com>
2017-12-18 16:32:06 +0100

    only offer other plugins when in advanced mode
    since settings in the popup will never change dynamically,
    we can just pass the result or storageGet directly, and don't need to
    bother with the Settings store (which asynchronously gets
    settings on instantiation so we'd have to introduce a mechanism
    to wait for that)

--- src/popup.js
+++ src/popup.js
@@ -14,32 +14,34 @@ import glamorous, { ThemeProvider } from 'glamorous'
 import delay from 'delay'
 import { Provider } from 'mobx-react'
 
-import { getTabs, getCurrentTab, selectTab, closeTab } from 'browser-api'
+import { getCurrentTab, storageGet } from 'browser-api'
 import ErrorBoundary from 'components/ErrorBoundary'
 import Tabulous from 'components/Tabulous'
-import settings from 'store/Settings'
 import Sources from 'store/Sources'
 
 import Tabs from 'plugins/Tabs'
 import Bookmarks from 'plugins/Bookmarks'
 import RecentlyClosed from 'plugins/RecentlyClosed'
 
-const sources = new Sources([Tabs, Bookmarks, RecentlyClosed])
-
-// the chrome extension window collapses to a tiny size
-// if we render immediately, so we'll delay by 10ms :/
-delay(10)
-  .then(getCurrentTab)
-  .then(({ index }) => {
-    sources.directObjects.setIndex(index)
-    render(
-      <Provider settings={settings}>
-        <ErrorBoundary settings={settings}>
-          <ThemeProvider theme={settings}>
-            <Tabulous settings={settings} sources={sources} />
-          </ThemeProvider>
-        </ErrorBoundary>
-      </Provider>,
-      window.document.getElementById('app-container')
-    )
-  })
+Promise.all([
+  getCurrentTab(),
+  storageGet(),
+  // the chrome extension window collapses to a tiny size
+  // if we render immediately, so we'll delay by 10ms :/
+  delay(10),
+]).then(([{ index }, settings]) => {
+  const sources = new Sources(
+    settings.advancedMode ? [Tabs, Bookmarks, RecentlyClosed] : [Tabs]
+  )
+  sources.directObjects.setIndex(index)
+  render(
+    <Provider settings={settings}>
+      <ErrorBoundary settings={settings}>
+        <ThemeProvider theme={settings}>
+          <Tabulous settings={settings} sources={sources} />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </Provider>,
+    window.document.getElementById('app-container')
+  )
+})
